4a6c64b0d0dbc90c9d0d102fd6b09d0c114214c3
Bashamega <adambashaahmednaji@gmail.com>
2024-06-05 19:38:31 +0300

    Nav integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { Nav } from "./components/Nav";
 export default function App(){
+  const [term, setTerm] = useState<string>('');
+
   return(
     <main>
-      <Nav/>
+      <Nav setVal={setTerm}/>
     </main>
   )
 }
