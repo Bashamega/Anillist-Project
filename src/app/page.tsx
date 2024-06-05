b4aaefd1dd1546bdf057ec76f99f7470a52b8f4c
Bashamega <adambashaahmednaji@gmail.com>
2024-06-05 20:16:34 +0300

    API Integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,89 @@
 "use client"
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Nav } from "../components/Nav";
 import { Grid } from "../components/grid";
 import { Pagination } from "../components/Pagination";
-export default function App(){
+interface PageData {
+  data: {
+    Page: {
+      pageInfo: {
+        total: number,
+        currentPage: number,
+        lastPage: number,
+        hasNextPage: boolean,
+        perPage: number
+      },
+      media: {
+        id: number,
+        title: {
+          romaji: string
+        }
+      }[]
+    }
+  }
+}
+export default function App() {
   const [term, setTerm] = useState<string>('');
-  const [currentPage, setCurrentPage] = useState<number>(1)
-  return(
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [error, setError] = useState<boolean>(false);
+  const [nextPage, setNextPage] = useState<boolean>(true);
+  const handleError = (e:any)=>{
+    console.log(e);
+    setError(true)
+  }
+  const handleData = (data:PageData)=>{
+    console.log(data);
+    setNextPage(data.data.Page.pageInfo.hasNextPage)
+  }
+  useEffect(() => {
+    var query = `
+query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+  Page (page: $page, perPage: $perPage) {
+    pageInfo {
+      total
+      currentPage
+      lastPage
+      hasNextPage
+      perPage
+    }
+    media (id: $id, search: $search) {
+      id
+      title {
+        romaji
+      }
+    }
+  }
+}
+`;
+
+    var variables = {
+      search: term || "",
+      page: currentPage,
+      perPage: 30
+    };
+
+    var url = 'https://graphql.anilist.co',
+      options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        })
+      };
+
+    fetch(url, options).then(res=>res.json())
+      .then(handleData)
+      .catch(handleError);
+  }, [currentPage, term])
+  return (
     <main>
-      <Nav setVal={setTerm}/>
-      <Grid/>
-      <Pagination page={currentPage} setPage={setCurrentPage}/>
+      <Nav setVal={setTerm} />
+      <Grid />
+      <Pagination page={currentPage} setPage={setCurrentPage} nextPage={nextPage} />
     </main>
   )
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { GrNext, GrPrevious } from "react-icons/gr";
-export function Pagination({page, setPage}:{page:number, setPage:React.Dispatch<React.SetStateAction<number>>}){
+export function Pagination({page, setPage, nextPage}:{page:number, setPage:React.Dispatch<React.SetStateAction<number>>, nextPage:boolean}){
 
     return(
         <footer className="bg-blue-500 text-2xl items-center text-white w-1/3 shadow-md rounded-lg absolute bottom-10 p-5 left-1/3 flex justify-evenly">
             <GrPrevious className={page==1?"text-slate-300":""} onClick={()=>setPage(page !=1? page-1: page)}/>
             {page}
-            <GrNext onClick={()=>setPage(page+1)}/>
+            <GrNext className={!nextPage?"text-slate-300":""} onClick={()=>setPage(nextPage? page+1: page)}/>
         </footer>
     )
 }
