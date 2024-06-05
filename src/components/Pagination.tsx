2cd06abb63fdbaa6cfc26b8d605eb1bf713e6853
Bashamega <adambashaahmednaji@gmail.com>
2024-06-05 19:57:05 +0300

    React icons

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { GrNext, GrPrevious } from "react-icons/gr";
 export function Pagination({page}:{page:number}){
     return(
         <footer className="bg-blue-500 text-white w-1/3 shadow-md rounded-lg absolute bottom-10 p-5 left-1/3 flex justify-evenly">
+            <GrPrevious/>
             {page}
+            <GrNext/>
         </footer>
     )
 }
