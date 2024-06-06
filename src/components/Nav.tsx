57c2592806b3abf5b5546ec111fcb60e9beb23d6
Bashamega <adambashaahmednaji@gmail.com>
2024-06-06 05:23:24 +0300

    Update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 
 export function Nav({setVal}:{setVal:React.Dispatch<React.SetStateAction<string>>}){
     const [localVal, setLocal] = useState<string>('');
-    const handleSubmit = ()=>{
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
         setVal(localVal)
     }
     return(
         <nav className="m-10 p-5 bg-blue-500 rounded-lg relative shadow-xl">
             <h1 className=" text-2xl text-white border p-1 rounded text-center">Anilist Project | Unofficial</h1>
             <section className=" p-5 absolute w-1/2 flex  left-1/4 bg-white rounded shadow-lg">
-                <input type="search" value={localVal} onChange={(e)=>{setLocal(e.target.value)}} className=" border-2 rounded shadow w-3/4 p-2 text-lg" placeholder="Search..."/>
-                <button onClick={handleSubmit} className="bg-blue-500 text-white font-bold py-2 px-4 w-1/4 ml-2 rounded hover:bg-blue-700">Search</button>
+                <form className="flex w-full" onSubmit={handleSubmit}>
+                <input required={true} type="search" value={localVal} onChange={(e)=>{setLocal(e.target.value)}} className=" border-2 rounded shadow w-3/4 p-2 text-lg" placeholder="Search..."/>
+                <button  className="bg-blue-500 text-white font-bold py-2 px-4 w-1/4 ml-2 rounded hover:bg-blue-700">Search</button>
+                </form>
             </section>
         </nav>
     )
