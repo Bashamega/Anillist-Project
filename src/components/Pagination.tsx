d3e1fb966f1edacb4988bbb59ea7f5668a010be7
Bashamega <adambashaahmednaji@gmail.com>
2024-06-06 04:48:04 +0300

    Pagination position

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,7 +3,7 @@ import { GrNext, GrPrevious } from "react-icons/gr";
 export function Pagination({page, setPage, nextPage}:{page:number, setPage:React.Dispatch<React.SetStateAction<number>>, nextPage:boolean}){
 
     return(
-        <footer className="bg-blue-500 text-2xl items-center text-white w-1/3 shadow-md rounded-lg absolute bottom-10 p-5 left-1/3 flex justify-evenly">
+        <footer className="bg-blue-500 text-2xl items-center text-white w-1/3 shadow-md rounded-lg absolute mb-10 p-5 left-1/3 flex justify-evenly">
             <GrPrevious className={page==1?"text-slate-300":""} onClick={()=>setPage(page !=1? page-1: page)}/>
             {page}
             <GrNext className={!nextPage?"text-slate-300":""} onClick={()=>setPage(nextPage? page+1: page)}/>
